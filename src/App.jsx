7379832e9bdfdc7a328d51cc3f2fd8fbeec8ca9b
Carla Paloma <carlapaloma@Carlas-MacBook-Air.local>
2021-09-21 10:11:20 -0700

    fecthing to heroku API

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import logo from "./logo.svg";
 
 const App = () => {
   const [books, setBooks] = useState([]);
+  console.log("component rendering now ------- ");
 
   useEffect(() => {
-    const fetchData = () => {
-      fetch("https://bookclub-books-api.herokuapp.com/books")
-        .then((response) => {
-          console.log(`response: `, response);
-          return response.json();
-        })
-        .then((books) => {
-          console.log(`json-ified response: `, books);
-          return setBooks(books);
-        });
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://bookclub-books-api.herokuapp.com/books"
+        );
+        console.log(`successful response: `, response);
+
+        const books = await response.json();
+        console.log(`json-ified response after successful response: `, books);
+        setBooks(books); // update array if response ok
+      } catch (errors) {
+        console.log(errors);
+      }
     };
     fetchData();
   }, []);
